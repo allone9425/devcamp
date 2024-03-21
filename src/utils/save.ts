494815b50a6a3d56e37f07bf5f3dbfbb88d089f9
allone9425 <allone9425@gmail.com>
2024-03-21 10:51:09 +0900

    feat : json server 설치 및 next auth 삭제

---
+++ src/utils/save.ts
@@ -0,0 +1,24 @@
+export interface AccountData {
+  username: string;
+  email: string;
+  phone: string;
+  role: string;
+  password: string;
+}
+export async function saveAccountData(
+  accountData: AccountData
+): Promise<AccountData> {
+  const response = await fetch("http://localhost:4000/accounts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(accountData),
+  });
+
+  if (!response.ok) {
+    throw new Error("API 호출에 실패했습니다.");
+  }
+
+  return await response.json();
+}
